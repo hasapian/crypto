981eb46f867af4eb34d55c108f5085516abca5eb
EC2 Default User <ec2-user@ip-172-31-18-148.us-east-2.compute.internal>
2020-12-24 21:54:35 +0000

    Need to check some things and all scenarios

--- node/app.js
+++ node/app.js
@@ -22,113 +22,92 @@ const coins = ['BTC','ETH','LINK','CRO','SXP','MATIC','RSR','VET','BLZ','DOT','A
 const CoinsEnum = {BTC:0,ETH:1,LINK:2,CRO:3,SXP:4,MATIC:5,RSR:6,VET:7,BLZ:8,DOT:9,ADA:10,CEL:11};
 Object.freeze(CoinsEnum);
 var deposits = new Array(coins.length).fill(0);
-var posessions = new Array(coins.length).fill(0);
-var trades = new Array(coins.length).fill(0);
+var holdings = new Array(coins.length).fill(0);
 var values = new Array(coins.length).fill(0);
 
-const sqlDeposits = "SELECT * FROM deposits"
-const sqlPosessions = "SELECT * FROM posessions"
-const sqlTrades = "SELECT * FROM trades"
+const sqlHoldings = "SELECT * FROM holdings"
 
-var depositResult,posessionsResult,tradesResult;
-var totalDeposits,stablecoins,stableDeposits;
+var holdingsResult;
+var totalDeposits,stableOrFiat;
 
-function myFilltable(result,x) {
-    if(x==1) {
-        totalDeposits = 0;
-        stableDeposits = 0;
-    }
+function myFilltable(result,usdtoeuro) {
+    totalDeposits = 0;
+    stableOrFiat = 0;
+
     for(i=0;i<result.length;i++) {
+        var depositAmount = result[i].deposit;
+        if(result[i].depositCurrency == 'USDT')
+            depositAmount = depositAmount * usdtoeuro;
+        if(result[i].totalDeposits)
+            totalDeposits+=depositAmount;
+
         var coinIndex = CoinsEnum[result[i].coin];
         var amount = result[i].amount;
-	    if(x==1)
-	        totalDeposits+=amount;
-        var coinIndex;
-        if(result[i].coin == 'USDT') {
-            if(x==1)
-                stableDeposits+=amount;
-            else if(x==2)
-                stablecoins = amount;
+            
+        if(result[i].coin == 'USDT' || result[i].coin == 'EUR') {
+            if(result[i].coin == 'USDT')
+                amount = amount * usdtoeuro;
+            stableOrFiat+=amount;
         }
         else {
-            switch(x) {
-                case 1:
-                    deposits[coinIndex]+=amount;
-                    break;
-                case 2:
-                    posessions[coinIndex]+=amount;
-                    break;
-                case 3:
-                    trades[coinIndex]+=amount;
-                    break;
-                default:
-                    //should do nothing
-            }
+            holdings[coinIndex]+=amount;
+            deposits[coinIndex]+=depositAmount;
         }
+            
     }
+
 }
 
 app.get('/', (req,res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
-    res.write("<h1>Profits:</h1>\n");
+    res.write("<h1>Work In progress</h1>\n");
     client.getQuotes({symbol: ['BTC,ETH,LINK,CRO,SXP,MATIC,RSR,DOT,VET,BLZ,ADA,CEL'], convert: priceIn}).then((prices) => {
-        db.query(sqlDeposits, function(err,result) {
-            if(err) throw err;
-            depositResult = result;
-            deposits.fill(0);
-            myFilltable(result,1);
-            db.query(sqlPosessions, function(err,result) {
+        exchange.convert({source: 'USD', target: 'EUR'}).then((result) => {
+            usdtoeuro = result.rate;
+            db.query(sqlHoldings, function(err,result) {
                 if(err) throw err;
-                posessionsResult = result;
-                posessions.fill(0);
-                myFilltable(result,2);
-                db.query(sqlTrades, function(err,result) {
-                    if(err) throw err;
-                    tradesResult = result;
-                    trades.fill(0);
-                    myFilltable(result,3);
-                    exchange.convert({source: 'USD', target: 'EUR'}).then((result) => {
-                        usdtoeuro = result.rate;
-                        var sumOfPosessions = 0;
-                        for(i=0;i<coins.length;i++){
-                            res.write("\n"+coins[i]);
-                            res.write("\nDeposits: "+(deposits[i] + trades[i] * usdtoeuro).toString()+"<br>");
-                            res.write("\nHoldings: "+posessions[i].toString()+"<br>");
-                            cmcprice = prices.data[coins[i]].quote[priceIn].price;
-                            res.write("\nPrice: "+cmcprice.toString()+"<br>");
-                            var tempValue = (priceIn == 'USD') ? posessions[i] * cmcprice * usdtoeuro : posessions[i] * cmcprice;
-                            values[i] = tempValue;
-                            res.write("\nProfit: "+(tempValue - ( deposits[i] + trades[i] * usdtoeuro )).toString()+"<br>");
-                            res.write("\n---------<br>");
-                            sumOfPosessions += tempValue;
-                            if(i==(coins.length-1)) { 
-                                res.write("Deposits:<br>")
-                                for(j=0;j<coins.length;j++) {
-                                    var percentage = ((deposits[j] + trades[j] * usdtoeuro) * 100) / (totalDeposits - stableDeposits);
-                                    res.write(coins[j]+": "+Math.round((percentage + Number.EPSILON) * 100) / 100+"%<br>");
-                                }
-                                res.write("---------<br>");
-                                res.write("Portfolio<br>");
-                                for(j=0;j<coins.length;j++) {
-                                    var percentage = (values[j] * 100) / (sumOfPosessions - stableDeposits);
-                                    res.write(coins[j]+": "+Math.round((percentage + Number.EPSILON) * 100) / 100+"%<br>");
-                                }
-                                res.write("---------<br>");
-                                res.write("Total Holdings Value: "+sumOfPosessions+"<br>");
-                                res.write("Total Deposits: "+totalDeposits+"<br>");
-                                res.write("<b>P&L: "+(sumOfPosessions - totalDeposits)+"</b><br>");
-				                res.write("(plus "+stablecoins+" stablecoins = "+stablecoins * usdtoeuro+" EURO)<br>");
-                                res.write('<a href="\add">Add holdings!</a><br>');
-                                res.write('<a href="\stable">Update Stablecoins!</a><br>');
-                                res.write('<a href="\\trade">Add new trade!</a><br>');                              
-                                res.end();
-                            } //end if
-                        } //end for
-                    }).catch(console.error)  //exchange
-                });
+                holdingsResult = result;
+                deposits.fill(0);
+                holdings.fill(0);
+                myFilltable(result,usdtoeuro);
+                var sumOfPosessions = 0;
+                for(i=0;i<coins.length;i++){
+                    res.write("\n"+coins[i]);
+                    res.write("\nDeposits: "+(deposits[i]).toString()+"<br>");
+                    res.write("\nHoldings: "+holdings[i].toString()+"<br>");
+                    cmcprice = prices.data[coins[i]].quote[priceIn].price;
+                    res.write("\nPrice: "+cmcprice.toString()+"<br>");
+                    var tempValue = (priceIn == 'USD') ? holdings[i] * cmcprice * usdtoeuro : holdings[i] * cmcprice;
+                    values[i] = tempValue;
+                    res.write("\nProfit: "+(tempValue - deposits[i]).toString()+"<br>");
+                    res.write("\n---------<br>");
+                    sumOfPosessions += tempValue;
+                    if(i==(coins.length-1)) { 
+                        res.write("Deposits:<br>")
+                        for(j=0;j<coins.length;j++) {
+                            var percentage = ((deposits[j]) * 100) / (totalDeposits - stableOrFiat);
+                            res.write(coins[j]+": "+Math.round((percentage + Number.EPSILON) * 100) / 100+"%<br>");
+                        }
+                        res.write("---------<br>");
+                        res.write("Portfolio<br>");
+                        for(j=0;j<coins.length;j++) {
+                            var percentage = (values[j] * 100) / (sumOfPosessions);
+                            res.write(coins[j]+": "+Math.round((percentage + Number.EPSILON) * 100) / 100+"%<br>");
+                        }
+                        res.write("---------<br>");
+                        res.write("Total Holdings Value: "+(sumOfPosessions + stableOrFiat)+"<br>");
+                        res.write("Total Deposits: "+totalDeposits+"<br>");
+                        res.write("<b>P&L: "+((sumOfPosessions + stableOrFiat) - totalDeposits)+"</b><br>");
+                        res.write("("+stableOrFiat+" EURO in Fiat or Stablecoins)<br>");
+                        // res.write('<a href="\add">Add holdings!</a><br>');
+                        // res.write('<a href="\stable">Update Stablecoins!</a><br>');
+                        // res.write('<a href="\\trade">Add new trade!</a><br>');                              
+                        res.end();
+                    } //end if
+                } //end for
             });
-        });
+        }).catch(console.error)  //exchange
     }).catch(console.error)
 });
 
@@ -174,37 +153,29 @@ app.post('/updateStablecoins', function (req,res) {
     });
 });
 
-app.get('/showPosessions', function (req,res) {
-    db.query("SELECT * FROM posessions", function(err,result) {
+app.get('/holdings', function (req,res) {
+    db.query("SELECT * FROM holdings", function(err,result) {
         if(err) throw err;
-	for(i=0;i<result.length;i++) {
-	        res.write(result[i].coin+": ");
-		res.write(result[i].amount+"\n");
-	}
-	res.end();
+        // for(i=0;i<result.length;i++) {
+        //         res.write(result[i].coin+": ");
+        //         res.write(result[i].amount+"\n");
+        // }
+        res.write("<table>");
+        res.write("<tr>");
+        for(var column in result[0]){
+            res.write("<td><label>" + column + "</label></td>");
+        }
+        res.write("</tr>");
+        for(var row in result){
+            res.write("<tr>");
+            for(var column in result[row]){
+                res.write("<td><label>" + result[row][column] + "</label></td>");       
+            }
+            res.write("</tr>");         
+        }
+        res.write("</table>");
+	    res.end();
     });
-});
-
-app.get('/showTrades', function (req,res) {
-	    db.query("SELECT * FROM trades", function(err,result) {
-		    if(err) throw err;
-		    for(i=0;i<result.length;i++) {
-			    res.write(result[i].coin+": ");
-			    res.write(result[i].amount+"\n");
-		    }
-		    res.end();
-		        });
-});
-
-app.get('/showDeposits', function (req,res) {
-	    db.query("SELECT * FROM deposits", function(err,result) {
-		    if(err) throw err;
-		    for(i=0;i<result.length;i++) {
-			    res.write(result[i].coin+": ");
-			    res.write(result[i].amount+"\n");
-		    }
-		    res.end();
-		        });
 });
 
 /*
