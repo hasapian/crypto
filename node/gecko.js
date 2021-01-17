2ba8b6669ff0acfb877ce2babf6723533573c736
EC2 Default User <ec2-user@ip-172-31-18-148.us-east-2.compute.internal>
2021-01-17 19:21:43 +0000

    Find id from symbol

--- node/gecko.js
+++ node/gecko.js
@@ -1,12 +1,20 @@
-const CoinGecko = require('coingecko-api');
- 
-const CoinGeckoClient = new CoinGecko();
+const CoinGecko = require('coingecko-api');
+ 
+const CoinGeckoClient = new CoinGecko();
 
-CoinGeckoClient.simple.price({
-		ids: ['bitcoin','ethereum'],
-		vs_currencies: ['eur', 'usd'],
-	}).then((results) => {
-		console.log(results.data.bitcoin.usd);
-	}).catch(console.error)
-
+CoinGeckoClient.coins.list().then((results) => {
+    for(i=0;i<results.data.length;i++) {
+	    if(results.data[i].symbol == 'blz'){
+		    console.log(i);
+		    console.log(results.data[i].id);
+		    var spyros = results.data[i].id;
+		    CoinGeckoClient.simple.price({
+			    ids:[spyros],
+			    vs_currencies:['usd']
+		    }).then((result) => {
+			    console.log(result.data[spyros].usd);
+		    }).catch(console.error)
+	    }
+    }
+}).catch(console.error)
 
