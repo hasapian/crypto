c6fbf7778f23fe84e5e23f13a32ed373eb3b7b8b
EC2 Default User <ec2-user@ip-172-31-18-148.us-east-2.compute.internal>
2021-01-18 19:59:56 +0000

    POC with gecko symbol and ids

--- node/verify.js
+++ node/verify.js
@@ -1,4 +1,8 @@
 var mysql = require("mysql");
+const CoinGecko = require('coingecko-api');
+	 
+const CoinGeckoClient = new CoinGecko();
+
 
 var con = mysql.createConnection({
   host: "mysql",
@@ -9,6 +13,36 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
+});
+
+con.query("select * from coins", function(err,result) {
+	if(err) throw err;
+	console.log(result);
+	var data = result;
+	var list,found,i;
+	var coinIds = [];
+	CoinGeckoClient.coins.list().then((results) => {
+	  for(j=0;j<data.length;j++) {
+	    console.log("j:"+j);
+	    list = results;
+	    found = false;
+	    i = 0;
+	    while((i < list.data.length) && (found == false)) {
+   		    if(list.data[i].symbol == data[j].coin){
+			    found = true;
+    			    coinIds.push(list.data[i].id);
+			    console.log(coinIds);
+    		    }
+		i++;
+    	    }
+	  }
+	  CoinGeckoClient.simple.price({
+		  ids:[coinIds],
+		  vs_currencies:['usd']
+	  }).then((result) => {
+		  console.log(result.data);
+	  }).catch(console.error)
+	}).catch(console.error)
 });
 
 module.exports = con;
