b776d139ebe0f363f1d9ea28b5327ddc32834945
EC2 Default User <ec2-user@ip-172-31-18-148.us-east-2.compute.internal>
2021-01-22 23:13:25 +0000

    We might be fine. Show prices now

--- node/ouf.js
+++ node/ouf.js
@@ -17,25 +17,46 @@ con.connect(function(err) {
 
 con.query("select * from coins", function(err,result) {
 	if(err) throw err;
-	console.log(result);
 	var data = result;
 	var found,i;
-	var coinIds = [];
-	CoinGeckoClient.coins.markets({per_page:[2],page:[1]}).then((markets) => {
-        i=0;
-        found=false;
-        while((i < markets.data.length) && (found == false)) {
-            if(markets.data[i].symbol == data[0].coin.toLowerCase()) {
-                found = true;
-                coinIds.push(markets.data[i].id);
-                console.log("In loop");
-                console.log(coinIds);
+    var coinIds = [];
+    var unfound = [];
+
+    CoinGeckoClient.coins.markets({per_page:[250],page:[1]}).then((markets) => {
+        for(j=0;j<data.length;j++) {
+            i=0;
+            found=false;
+            while((i < markets.data.length) && (found == false)) {
+                if(markets.data[i].symbol == data[j].coin.toLowerCase()) {
+                    found = true;
+                    coinIds.push(markets.data[i].id);
+                }
+                i++;
             }
-            i++;
+            if(!found) {
+                unfound.push(data[j])
+            }
         }
-        console.log("Loop end");
-        console.log(coinIds);
-	}).catch(console.error)
+	console.log("Found:");
+	console.log(coinIds);
+        console.log("Unfound:");
+        console.log(unfound);
+        CoinGeckoClient.coins.markets({per_page:[250],page:[2]}).then((markets2) => {
+            for(j=0;j<unfound.length;j++) {
+                i=0;
+                found=false;
+                while((i < markets2.data.length) && (found == false)) {
+                    if(markets2.data[i].symbol == unfound[j].coin.toLowerCase()) {
+                        found = true;
+                        coinIds.push(markets2.data[i].id);
+                        console.log("In loop2");
+                        console.log(coinIds);
+                    }
+                    i++;
+                }
+            }
+        }).catch(console.error)
+    }).catch(console.error)
 });
 
 module.exports = con;
