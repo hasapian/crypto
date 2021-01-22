e98fe964c6f13a3e07e2b1fe049dcbe57462894c
EC2 Default User <ec2-user@ip-172-31-18-148.us-east-2.compute.internal>
2021-01-22 16:24:00 +0000

    Added coins in table with init

--- node/coinsInit.js
+++ node/coinsInit.js
@@ -7,7 +7,8 @@ var con = mysql.createConnection({
   database: "cryptoDB"
 });
 
-var sql = "INSERT INTO coins (coin) VALUES ('BTC'),('ETH'),('LINK'),('CRO'),('SXP'),('MATIC'),('RSR'),('VET'),('BLZ'),('DOT'),('ADA'),('CEL'),('UNI'),('GRT'),('ZIL'),('AAVE'),('XLM'),('SNX'),('COMP');"
+var sql = "INSERT INTO coins (id,coin) VALUES (0,'BTC'),(1,'ETH'),(2,'LINK'),(3,'CRO'),(4,'SXP'),(5,'MATIC'),(6,'RSR'),(7,'VET'),(8,'BLZ'),(9,'DOT'),(10,'ADA'),(11,'CEL'),(12,'UNI'),(13,'GRT'),(14,'ZIL'),"+
+"(15,'AAVE'),(16,'XLM'),(17,'SNX'),(18,'COMP');"
 
 con.connect(function(err) {
   if (err) throw err;
